bdf07c3255c2d1f439527952983676ba6fd47510
MaximoVerdi <maximoverdi21@mail.com>
2025-05-07 11:29:48 -0300

    Add: Funcion updateSneaker para encontrar sneaker por id

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,9 @@ const URI_DB = process.env.URI_DB || "";
 const connectDB = async () => {
     try {
         await connect(URI_DB)
-        console.log("Conectado a la DB")
+        console.log("Connected to DB")
     } catch (error: any) {
-        console.log("Error conectando a la DB")
+        console.log("Error connecting to DB", error.message)
     }
 }
 
@@ -30,25 +30,73 @@ const sneakersSchema = new Schema({
 const Sneaker = model("Sneaker", sneakersSchema)
 
 
-const crearSneaker = async (newSneaker: ISneaker) => {
+const createSneaker = async (newSneaker: ISneaker) => {
     try {
         const {model, color, price, stock} = newSneaker
         if (!model || !color || !price || !stock) {
-            return ({succes: false, message: "Faltan datos"})
+            return ({succes: false, message: "Invalid Data"})
         }
 
 
         const existingSneaker = await Sneaker.findOne({color, model});
         if (existingSneaker) {
-            return ({succes: false, message: "Ya existe un sneaker con ese color y modelo"})
+            return ({succes: false, message: "Already exists a sneaker for that model and color"})
         }
         const newSneakerToDb = new Sneaker({model, color, price, stock})
         await newSneakerToDb.save()
         return {
             succes: true,
             data: newSneakerToDb,
-            message: "Sneaker creado correctamente"
+            message: "Sneaker created successfully"
         }
+    } catch (error: any) {
+        return {
+            succes: false,
+            message: error.message
+        }
+    }
+}
+
+const findSneakerByColor = async (color: string) => { 
+    try {
+        const sneakers = await Sneaker.find({color})
+        if (sneakers.length === 0) {
+            return ({succes: false, message: "No sneakers for that color"})
+        }
+        return {
+            succes: true,
+            data: sneakers,
+            message: "Sneaker found"
+        }
+    } catch (error: any) {
+        return {
+            succes: false,
+            message: error.message
+        }
+    }
+}
+
+const updateSneaker = async (id: string, newSneaker: ISneaker) => {
+    try {
+        const {model, color, price, stock} = newSneaker
+        if (!model || !color || !price || !stock) {
+            return ({succes: false, message: "Sneaker not found"})
+        }
+
+        const updatedSneaker = await Sneaker.findByIdAndUpdate(id, {model, color, price, stock}, {new: true})
+
+        if (!updatedSneaker) {
+            return { succes: false, message: "Sneaker not found" };
+        }
+
+        await updatedSneaker.save()
+        
+        return {
+            succes: true,
+            data: updatedSneaker,
+            message: "Sneaker updated successfully"
+        }
+        
     } catch (error: any) {
         return {
             succes: false,
@@ -59,7 +107,7 @@ const crearSneaker = async (newSneaker: ISneaker) => {
 
 const main = async () => {
     connectDB()
-    const result = await crearSneaker({model: "Jordan 1", color: "Chicago", price: 8500, stock: 2})
+    const result = await updateSneaker("681b63329a858737c15492ab", {model: "Jordan 1", color: "Chicago", price: 7500, stock: 3})
     console.log(result)
 }
 
